7747f7e5276caa26969d4754d75be639ecd2090b
Bharti Shinde <Bharti.Shinde01@student.csulb.edu>
2021-03-08 11:15:14 -0800

    Chapter4

---
+++ js/ex1.js
@@ -0,0 +1,12 @@
+// carousel
+
+//let turn = 10;
+//while (turn <= 10) {
+  //console.log(turn);
+  //turn++;
+//}
+
+let i = prompt("Please enter the number of turns");
+for (i = 10; i <=10; i++) {
+  console.log(`The number of turn is ${i}`);
+}
---
+++ js/ex2.js
@@ -0,0 +1,9 @@
+// parity
+let number = prompt("Enter a number of your choice")
+while (number <= 10){
+  if (number % 2 === 0){
+    console.log(`${number} is even`)
+  } else{
+    console.log(`${number} is odd`)
+  }
+};
